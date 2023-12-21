36ea0a328001029fdffa00ee7e3aeaf5da863709
B4MBANG-94NT3NG <bambangtriraharjo1207@gmail.com>
2023-12-21 18:05:26 +0700

    test

---
+++ settings.js
@@ -0,0 +1,146 @@
+const chalk = require("chalk")
+const fs = require("fs")
+
+//aumto presence update
+global.autoReading = true //auto recording (true to on, false to off)
+global.autoTyping = false //auto tying (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
+global.autoblockmorroco = false //auto block 212 (true to on, false to off)
+global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
+global.antispam = true //auto kick spammer (true to on, false to off)
+
+
+//if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
+global.keyopenai = "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"
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.ownernomer = "6287732200388" //ur owner number2
+global.ownername = "Owner_Yaemiko" //ur owner name
+global.ytname = "BTR CHANNEL" //ur yt chanel name
+global.socialm = "Someone.ex_" //ur github or insta name
+global.location = "Brebes, Jawa Tengah, Indonesia" //ur location
+
+//Note!!!
+//PENGGUNAAN APIKEY
+//CUMA BEBERAPA FITUR AJA
+//YANG LAIN SCRAPE
+
+//new
+global.lol = "7a42b0bf3636288484cfe9a7" //APIKEY LOLHUMAN
+global.apikicode = "Dzsyacans" //APIKEY KIICODE
+global.apikimz = "Hp53y3qMIX"
+global.botname = "Yaemiko_Bot"
+global.ownernumber = '6287732200388'
+global.ownername = 'Owner_Yaemiko'
+global.owner = 'Bambang Tri Raharjo'
+global.ownerNumber = ["6287732200388@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@bambangtriraharjo9396"
+global.websitex = "https://youtube.com/bambangtriraharjo1207"
+global.wagc = "https://chat.whatsapp.com/Dq7Pm9pXXWOKxtR3ROMYkv"
+global.themeemoji = '🪀'
+global.wm = "Yaemiko_Bot"
+global.botscript = '100% No Enc Chat Owner' //script link
+global.packname = "Sticker By"
+global.author = "Bambang Tri Raharjo"
+global.creator = "6287732200388@s.whatsapp.net"
+global.prefa = ['','!','.','#','&']
+global.sessionName = 'session'
+global.hituet = 0
+
+//media target
+global.thum = fs.readFileSync("./YaeMedia/theme/cheemspic.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./YaeMedia/theme/cheemspic.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./YaeMedia/theme/cheemspic.jpg") //ur error pic
+global.thumb = fs.readFileSync("./YaeMedia/theme/cheemspic.jpg") //ur thumb pic
+global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60' //default pp wa
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+global.rpg = {
+    emoticon(string) {
+        string = string.toLowerCase()
+        let emot = {
+            level: '📊',
+            limit: '🎫',
+            health: '❤️',
+            exp: '✨',
+            atm: '💳',
+            money: '💰',
+            bank: '🏦',
+            potion: '🥤',
+            diamond: '💎',
+            common: '📦',
+            uncommon: '🛍️',
+            mythic: '🎁',
+            legendary: '🗃️',
+            superior: '💼',
+            pet: '🔖',
+            trash: '🗑',
+            armor: '🥼',
+            sword: '⚔️',
+            pickaxe: '⛏️',
+            fishingrod: '🎣',
+            wood: '🪵',
+            rock: '🪨',
+            string: '🕸️',
+            horse: '🐴',
+            cat: '🐱',
+            dog: '🐶',
+            fox: '🦊',
+            robo: '🤖',
+            petfood: '🍖',
+            iron: '⛓️',
+            gold: '🪙',
+            emerald: '❇️',
+            upgrader: '🧰',
+            bibitanggur: '🌱',
+            bibitjeruk: '🌿',
+            bibitapel: '☘️',
+            bibitmangga: '🍀',
+            bibitpisang: '🌴',
+            anggur: '🍇',
+            jeruk: '🍊',
+            apel: '🍎',
+            mangga: '🥭',
+            pisang: '🍌',
+            botol: '🍾',
+            kardus: '📦',
+            kaleng: '🏮',
+            plastik: '📜',
+            gelas: '🧋',
+            chip: '♋',
+            umpan: '🪱',
+            skata: '🧩'
+        }
+        let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+        if (!results.length) return ''
+        else return emot[results[0][0]]
+    }
+}
+
+
+//messages
+global.mess = {
+    success: 'ini dia bg!', 
+    premium: 'maaf anda bukan pengguna premium',
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
